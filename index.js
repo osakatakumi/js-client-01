fc6b12e5e6d4f41bb8a6402922331a2feecbe929
osakatakumi <osuka0309@gmail.com>
2019-08-23 13:15:37 +0900

    夏休み　課題

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const tarea = document.getElementById('tarea');
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
 const btn = document.getElementById('btn');
 const result = document.getElementById('result');
 
 btn.addEventListener('click',()=>{
-    let findtxt = input1.value;
-    let reptxt = input2.value;
-    let tagtxt = tarea.value;
+    var number1 = input1.value;
+    var number2 = input2.value;
 
-    findtxt = new RegExp(findtxt, 'g');
-    tagtxt = tagtxt.replace(findtxt, reptxt);
-
-    result.innerText = tagtxt;
+    keisan = number2 / (number1 * number1);
+    keisan = Math.round(keisan);
+    result.innerText = keisan;
 });
